d9d10edc4e25daf2086b3032ed4a4fb899449d46
brownglobalofficial-glitch <brownglobalofficial@gmail.com>
2026-08-01 14:46:54 -0400

    Improve Reach navigation in src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,4 +4,6 @@ import App from "./App";
 import "./styles.css";
 import "./brand.css";
 import "./account.css";
+import "./navigation.css";
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
+
