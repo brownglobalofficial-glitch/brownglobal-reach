6ef9000ed06b8f9a66d121dace3b321cd82a6360
brownglobalofficial-glitch <brownglobalofficial@gmail.com>
2026-07-31 23:48:51 -0400

    Add shared BrownGlobal account experience

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,5 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
 import "./brand.css";
+import "./account.css";
 createRoot(document.getElementById("root")!).render(<StrictMode><App /></StrictMode>);
-
